2a233f4d9255b88864370567cf3c1e6229a55921
Johnny <yuhangyang74@gmail.com>
2022-04-08 13:08:29 +0800

    Text updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>this is my change</label>
+          <label>Test update</label>
           <input
             type="text"
             value={term}
